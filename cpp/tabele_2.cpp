c6e77df60d2ab7dafff14c270f08ff50714e737b
majkel1703 <makjkel1703@o2.pl>
2017-10-25 12:35:04 +0200

    tabele v2

--- cpp/tabele_2.cpp
+++ cpp/tabele_2.cpp
@@ -3,8 +3,35 @@
 */
 
  #include <iostream>
+ #include <cmath>
 
 using namespace std;
+
+void pobierz_dane(int tab[], int rozmiar)
+{
+    cout<< "Podaj " << rozmiar<< " ocen (0-6): "<<endl;
+
+    for(int i=0; i<rozmiar; i++)
+    {
+        cin>> tab[i];
+    }
+}
+
+int sumuj(int tab[], int rozmiar)
+{
+    int suma=0;
+    for(int i=0; i<rozmiar; i++)
+    {
+        suma+=tab[i];
+        cout<< tab[i]<<", ";
+    }
+    return suma;
+}
+
+float dziel(int tab[], int rozmiar)
+{
+    return sumuj(tab, rozmiar)/float(rozmiar);
+}
 
 int main(int argc, char **argv)
 {
@@ -14,25 +41,19 @@ int main(int argc, char **argv)
     cin>> rozmiar;
 
     int liczby[rozmiar];
-    int suma=0;
 
-    cout<< "Podaj " << rozmiar<< " ocen (0-6): "<<endl;
-
-    for(int i=0; i<rozmiar; i++)
-    {
-        cin>> liczby[i];
-    }
+    pobierz_dane(liczby, rozmiar);
 
     cout<<"Podane oceny: ";
 
-    for(int i=0; i<rozmiar; i++)
-    {
-        suma+=liczby[i];
-        cout<< liczby[i]<<", ";
-    }
+    //~for(int i=0; i<rozmiar; i++)
+    //~{
+        //~suma+=liczby[i];
+        //~cout<< liczby[i]<<", ";
+    //~}
 
-    cout<<endl<<"Suma ocen: "<< suma<<endl;
-    cout<<"Średnia: "<< float(suma)/float(rozmiar)<<endl;
+    cout<<endl<<"Suma ocen: "<< sumuj(liczby, rozmiar)<<endl;
+    cout<<"Średnia: "<< dziel(liczby, rozmiar)<<endl;
 
     return 0;
 }
