7ccdeb33899caec81f36a8ffc7e72929314699ce
majkel1703 <makjkel1703@o2.pl>
2017-11-08 12:36:08 +0100

    Python

---
+++ python/mlotek.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from random import randint
+
+
+def losuj(ileliczb, maksliczb):
+    liczby = []
+    ile = 0 # ilośc wylososwanych liczb
+    while ile < ileliczb:
+    # for i in range(ileliczb):
+        liczba = randint(0, maksliczb)
+        if liczby.count(liczba) == 0:
+            liczby.append(liczba)
+            ile += 1
+    print(liczby)
+
+
+def main(args):
+    ileliczb = int(input("Ile liczb chcesz zgadnąć? "))
+    maksliczb = int(input("Podaj górny zakres: "))
+
+    losuj(ileliczb, maksliczb)
+
+    typy = set()  # pusty zbiór
+    for i in range(ileliczb):
+        typ = input("Podaj typ: ")
+        typy.add(typ)
+
+    print(typy)
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
