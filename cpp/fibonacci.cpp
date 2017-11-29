3e9babad32efedda2ca202626448efb81fb0abfa
rosolczolgmakaron <mateuszsekulski83@gmail.com>
2017-11-29 12:36:10 +0100

    fibonacci na dwa sposoby

---
+++ cpp/fibonacci.cpp
@@ -0,0 +1,31 @@
+/*
+ * fibonacci
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int fib_iter(int n)
+{
+    int a = 0;
+    int a = 1;
+    if (n==0)
+    {
+        cout << 0;
+        return 0
+    }
+    cout << a;
+    for(int i==2; i<n; i++)
+    {
+        
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+
+    return 0
+}
--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def fib_iter(n):
     """
     Funkcja wyświetla kolejne wyrazy ciągu fibonacciego.
@@ -10,20 +11,38 @@ def fib_iter(n):
     F(n) = F(n-2) + F(n-1) dla n>1
     """
     a, b = (0, 1)
-    if n = 0:
-        print a
+    if n == 0:
+        print(a)
         return a
-    elif n =1:
-        print b
-        return b
+    # elif n == 1:
+        # print(b)
+        # return b
+    print(a)
+    print(b)
     for i in range(2, n):
-        b+= a
-        a= tmp
+        # tmp = b
+        # b += a
+        # a = tmp
+        a, b = b, a + b
+        print(a, " Wyraz", i + 1, ": ", b, "ILoraz: ", b / a)
 
     return b
 
 
+def fib_iter_2(n):
+    a, b = (0, 1)
+    i = 2
+    while n > 0:
+        n -= 1
+        i += 1
+        a, b = b, a + b
+        print(a, b, "Wyraz", i, ":", b, "  ILoraz: ", b / a)
+
+
 def main(args):
+
+    n = int(input("Podaj zakres: "))
+    print(fib_iter_2(n))
     return 0
 
 
