797e28fd2e229278f823316f917612055b513710
rosolczolgmakaron <mateuszsekulski83@gmail.com>
2018-01-26 11:43:50 +0100

    dec2bin algorytm cpp

---
+++ cpp/dec2bin.cpp
@@ -0,0 +1,41 @@
+/*
+ * dec2bin.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    char znakA = 'A';
+    char znakB = 'B';
+    int l14 = 14;
+    int l15 = 15;
+    
+    cout << (int)znakA << (int)znakB << endl;
+    cout << (char)l14 << (char)l15 << endl;
+	int liczba = 0;
+    int podstawa = 0;
+    int reszty[16];
+    int i =0;
+    
+    cout << "Podaj liczbę i podstawę: ";
+    cin >> liczba >> podstawa;
+    do{
+        reszty[i] =  liczba%podstawa;
+        liczba = liczba / podstawa;
+        i++;
+    } while(liczba>0);
+    
+    //~for (int j = i - 1; j >= 0; j--){
+        //~cout << reszty[j];
+    //~}
+    
+    while(i-1 >= 0){
+        i--;
+        cout << reszty[i];
+    }
+	return 0;
+}
+
