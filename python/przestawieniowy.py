ab56ad27039fa45297d65e29cfffeee39e9cff77
rosolczolgmakaron <mateuszsekulski83@gmail.com>
2018-02-09 11:43:20 +0100

    visioner 09.02.2018

--- python/przestawieniowy.py
+++ python/przestawieniowy.py
@@ -3,29 +3,44 @@
 
 
 def szyfruj(tekst, klucz):
-    tekst += (klucz - (len(tekst) % klucz)) * "."
-    # reszta = len(tekst) % klucz
+    szyfrogram = ""
+    reszta = len(tekst) % klucz
+
     if reszta:
         tekst += (klucz - reszta) * "."
-        print(tekst)
-    # print(tekst)
-        for i in range(klucz):
-            # print(tekst(i + ((i-1) * klucz)))
-            for j in range(int(len(tekst) / klucz)):
-                print(tekst[i + j * klucz])
-        return ""
+
+    for i in range(klucz):
+        for j in range(int(len(tekst) / klucz)):
+            szyfrogram += tekst[i + j * klucz]
+    return szyfrogram
+
+
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    for i in range(int(len(szyfrogram) / klucz)):
+        for j in range(klucz):
+            tekst += szyfrogram[i + (j * int(len(szyfrogram) / klucz))]
+            tekst = tekst.replace(".", "")
+
+    return tekst
+
 
 def main(args):
     tekst = input("Podaj tekst: ")
+
     klucz = int(input("Podaj klucz: "))
-    while(2 * klucz > len(tekst)):
+    # while (2 * klucz > len(tekst)):
+    while klucz > (0.5 * len(tekst)):
         klucz = int(input("Podaj klucz: "))
+
     szyfrogram = szyfruj(tekst, klucz)
-    print(szyfrogram)
-    # print(deszyfruj(szyfrogram, klucz))
+    print("Zaszyfrowany: ", szyfrogram)
+    deszyfrowany = deszyfruj(szyfrogram, klucz)
+    print("Deszyfrowany: ", deszyfrowany)
+
     return 0
 
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
