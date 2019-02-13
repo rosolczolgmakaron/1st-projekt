3a1e2cb5a6336ff2084e220a4b3bdc5a7c42563f
rosolczolgmakaron <mateuszsekulski83@gmail.com>
2019-02-13 09:41:14 +0100

    struktury wektor

--- cpp/wektor.cpp
+++ cpp/wektor.cpp
@@ -1,18 +1,48 @@
 #include <iostream>
 
+using namespace std;
+
+struct punkt
+{
+    int x;
+    int y;
+};
+
+struct wektor
+{
+    punkt pp;
+    punkt pk;
+};
+
+wektor getWektor()
+{
+    wektor w1;
+    cout << "Podaj x, y punktu początkowego:\n";
+    cin >> w1.pp.x;
+    cin >> w1.pp.y;
+    cout << "Podaj x, y punktu końcowego:\n";
+    cin >> w1.pk.x;
+    cin >> w1.pk.y;
+    
+    return w1;
+}
+
+punkt wylicz_srodek(wektor w)
+{
+    punkt ps;
+    ps.x = (w.pk.x + w.pp.x)/2;
+    ps.y = (w.pk.y + w.pp.y)/2;
+    return ps;
+}
+
 int main(int argc, char **argv)
 {
-	p1 = (x,y);
-    p2 = (a, b);
-    cout << "Podaj współrzędne pierwszego punktu: " << endl;
-    cin >> x >> y;
-    cout << "Podaj współrzędne drugiego punktu: " << endl:
-    cin >> a >> b;
-    wektor = (x-a, y-b)
-    polowa = ((x-a)/2, (y-b)/2)
-    cout << wektor << endl;
-    cout << polowa << endl; 
-    
+    wektor w;
+    punkt ps;
+    w = getWektor();
+    ps = wylicz_srodek(w);
+    cout << "Wektor: " << "(" << w.pp.x << ", " << w.pk.y << ")" << endl;
+    cout << "Środek: " << ps.x << ", " << ps.y << endl;
 	return 0;
 }
 
