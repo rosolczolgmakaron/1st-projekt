7ebd1ed425248dc7e85115887420139205a2d872
rosolczolgmakaron <mateuszsekulski83@gmail.com>
2019-02-20 09:40:44 +0100

    wskaźniki

---
+++ cpp/kodascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	char z;
+    int kodascii;
+    z = 'CR';
+    kodascii = (int)z;
+    cout << kodascii;
+	return 0;
+}
+
---
+++ cpp/wskazniki.cpp
@@ -0,0 +1,23 @@
+/*
+ * wskazniki.cpp
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int i = 13;
+    float j = 12.09;
+    int *wsk1; // deklaracja wskaźnika do liczby całkowitej
+    float *wsk2;
+    wsk1 = &i;
+    wsk2 = &j;
+    cout << i << endl;
+    cout << wsk1 << endl;
+    cout << *wsk1 << endl;
+    *wsk1 = (int)*wsk2;
+    cout << *wsk1 << endl;
+    return 0;
+}
+
